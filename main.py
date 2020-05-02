c0a408500c6cd9314c400c05223b7a457274011d
offish <overutilization@gmail.com>
2020-05-02 23:49:19 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+from dist.config import GAMES, VIDEO_LENGTH, CLIP_PATH
+from dist.other import create_video_config, get_date
+from dist.clips import get_clips, download_clips
+from dist.upload import upload_video_to_youtube
+from dist.video import render
+from dist.logging import log
+from os.path import isdir
+from pathlib import Path
+from time import sleep
+
+
+while True:
+
+    for game in GAMES:
+        
+        path = CLIP_PATH.format(get_date(), game)
+
+        if not isdir(path):
+            tries = 3
+
+            for i in range(tries):
+
+                Path(path).mkdir(parents=True, exist_ok=True)
+
+                clips = get_clips(game, VIDEO_LENGTH, path)
+
+                if clips != False:
+                    log('info', f'Starting to make a video for {game}')
+                    names = download_clips(clips, VIDEO_LENGTH, path)
+                    config = create_video_config(game, names)
+                    
+                    render(game, path)
+                    upload_video_to_youtube(config)
+
+                    del names
+                    del config
+                    break
+
+                else:
+                    log('error', f'There was an error or timeout on Twitch\'s end, retrying... {i}/{tries}')
+
+        else:
+            log('info', f'Already made a video for {game}. Rechecking in an hour.')
+
+    sleep(3600)
+    
