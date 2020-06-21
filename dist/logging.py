f3baeafea662e3d8661102e54c7a02a74ea3e56e
offish <overutilization@gmail.com>
2020-06-21 19:34:42 +0200

    Update logging.py

--- dist/logging.py
+++ dist/logging.py
@@ -2,23 +2,23 @@ from datetime import datetime
 
 import colorama
 
-
 colorama.init()
 
+class log:
+    white = '\033[0m'
+    green = '\033[1;32;40m'
+    red = '\033[1;31;40m'
+    purple = '\033[1;35;40m'
 
-TYPE = {
-    "warn": "\033[1;35;40m",
-    "info": "\033[1;32;40m",
-    "error": "\033[1;31;40m",
-    "normal": "\033[0m"
-}
+    def info(self, text: str):
+        self.log(self.green, 'info', text)
 
+    def error(self, text: str):
+        self.log(self.red, 'error', text)
 
-def log(sort: str, to_print: str):
-    time = datetime.now().time().strftime('%H:%M:%S')
+    def warn(self, text: str):
+        self.log(self.purple, 'warn', text)
 
-    if sort in TYPE:
-        print(f"{TYPE['info'] + 'twitchtube'} {TYPE['normal'] + '| ' + time } - {TYPE[sort] + sort}: {TYPE['normal'] + to_print}")
-    else:
-        print(f'{sort} not in TYPE. {to_print}')
-
+    def log(self, color, sort, text):
+        time = datetime.now().time().strftime('%H:%M:%S')
+        print(f'{self.green}twitchtube {self.white}| {time} - {color + sort}{self.white}: {text}')
