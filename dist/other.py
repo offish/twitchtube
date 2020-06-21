44e9639686e3272b6aa043de162fbc50864f302f
offish <overutilization@gmail.com>
2020-06-21 19:35:09 +0200

    Update other.py

--- dist/other.py
+++ dist/other.py
@@ -1,22 +1,21 @@
 from datetime import date
 import json
 
-from .config import TAGS, DESCRIPTIONS, CLIP_PATH
+from .config import TAGS, DESCRIPTIONS, CLIP_PATH, TITLE
 
 
 def get_date():
-    return date.today().strftime("%b-%d-%Y")
+    return date.today().strftime('%b-%d-%Y')
 
 
 def create_video_config(game, streamers):
     config = {
-        "category": 20,
-        "keywords": get_tags(game),
-        "description": get_description(game, streamers),
-        "title": get_title(game),
-        "file": get_file(game)
+        'category': 20,
+        'keywords': get_tags(game),
+        'description': get_description(game, streamers),
+        'title': get_title(game),
+        'file': get_file(game)
     }
-
     return config
 
 
@@ -25,7 +24,7 @@ def get_tags(game):
 
 
 def get_description(game, streamers):
-    names = "Streamers in this video:\n"
+    names = 'Streamers in this video:\n'
     
     for name in streamers:
         names += f'https://twitch.tv/{name}\n'
@@ -34,11 +33,14 @@ def get_description(game, streamers):
 
 
 def get_title(game):
+    if TITLE:
+        return TITLE
+
     title = json.loads(open(f'{CLIP_PATH.format(get_date(), game)}/clips.json', 'r').read())
 
     for i in title:
-        return f"{title[i]['title']} - {game} Highlights #"
+        return '{} - {} Highlights #'.format(title[i]['title'], game)
 
 
 def get_file(game):
-    return f"{CLIP_PATH.format(get_date(), game)}/rendered.mp4"
+    return f'{CLIP_PATH.format(get_date(), game)}/rendered.mp4'
