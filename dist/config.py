ac5a79fa74c30b8d18a402814396cbdd0a1178d0
offish <overutilization@gmail.com>
2020-07-26 16:51:19 +0200

    Update config.py

--- dist/config.py
+++ dist/config.py
@@ -38,14 +38,12 @@ CATEGORY = 20 # 20 for Gaming
 
 # YouTube Tags
 TAGS = {
-#    'Counter-Strike: Global Offensive': 'top csgo twitch clips,best csgo twitch clips,csgo top twitch clips,csgo daily twitch clips,crudecs,crude csgo,most viewed twitch clips csgo,twitch csgo videos,most watched csgo twitch clips ever,csgo twitch clip compilation,twitch csgo clips compilation,csgo clips twitch,csgo clips reddit,csgo clips download,csgo clips in desc,csgo twitch highlights,twitch cs go highlights,twitch highlights csgo,competitive cs go clips,csgo awp wildfire stattrak',
     'Rust': 'rust, rust twitch, rust twitch clips',
-    'Team Fortress 2': 'tf2, tf2 twitch, tf2 twitch clips',
+    'Team Fortress 2': 'tf2, tf2 twitch, tf2 twitch clips'
 }
 
 # YouTube Descriptions
 DESCRIPTIONS = {
-#   'Counter-Strike: Global Offensive': ',    
     'Rust': 'The most viewed RUST Twitch clips today. Daily RUST Twitch clip compilations.\n\n{}\n#RUST #RustHighlights #RustTwitch',
     'Team Fortress 2': 'The most viewed TF2 Twitch clips today. Daily TF2 Twitch clip compilations.\n\n{}\n#TF2 #TF2Highlights #TeamFortress2'
 }
