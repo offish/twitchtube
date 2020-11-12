ea827140ccf2bbe677b7066a34b721b6931941b5
offish <overutilization@gmail.com>
2020-11-12 20:35:09 +0100

    Update config.py

--- dist/config.py
+++ dist/config.py
@@ -14,11 +14,14 @@ CLIP_PATH = PATH + '/clips/{}/{}'
 
 
 # Video
-GAMES = ['Rust', 'Team Fortress 2']
+GAMES = ['Just Chatting', 'Team Fortress 2']
 VIDEO_LENGTH = 10.5 # in minutes (doesn't always work for some reason)
 FRAMES = 30 # Frames per second 30 or 60
 RESOLUTION = (720, 1280) # (height, width) for 1080p: (1080, 1920)
 
+# Other
+UPLOAD_TO_YOUTUBE = True # Whether or not the video should upload to YouTube after rendering (True/False)
+TIMEOUT = 3600 # How often it should check if it has made a video today (in seconds)
 
 # Twitch
 
@@ -38,12 +41,13 @@ CATEGORY = 20 # 20 for Gaming
 
 # YouTube Tags
 TAGS = {
-    'Rust': 'rust, rust twitch, rust twitch clips',
+    'Just Chatting': 'just chatting, just chatting clips, just chatting twitch clips',
     'Team Fortress 2': 'tf2, tf2 twitch, tf2 twitch clips'
 }
 
 # YouTube Descriptions
 DESCRIPTIONS = {
-    'Rust': 'The most viewed RUST Twitch clips today. Daily RUST Twitch clip compilations.\n\n{}\n#RUST #RustHighlights #RustTwitch',
-    'Team Fortress 2': 'The most viewed TF2 Twitch clips today. Daily TF2 Twitch clip compilations.\n\n{}\n#TF2 #TF2Highlights #TeamFortress2'
+    'Just Chatting': 'Just Chatting twitch clips \n\n{}\n',
+    'Team Fortress 2': 'TF2 twitch clips\n\n{}\n' 
 }
+# {} will be replaced with a list of streamer names
