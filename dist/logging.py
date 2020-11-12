f9fe755091c1504399f54cf87557c133b33a9a83
offish <overutilization@gmail.com>
2020-11-12 20:34:14 +0100

    Update logging.py

--- dist/logging.py
+++ dist/logging.py
@@ -1,24 +1,21 @@
 from datetime import datetime
 
-import colorama
+from colorama import Fore as f
+from colorama import init
 
-colorama.init()
+init()
 
 class log:
-    white = '\033[0m'
-    green = '\033[1;32;40m'
-    red = '\033[1;31;40m'
-    purple = '\033[1;35;40m'
 
     def info(self, text: str):
-        self.log(self.green, 'info', text)
+        self.log(f.GREEN, 'info', text)
 
     def error(self, text: str):
-        self.log(self.red, 'error', text)
+        self.log(f.RED, 'error', text)
 
     def warn(self, text: str):
-        self.log(self.purple, 'warn', text)
+        self.log(f.MAGENTA, 'warn', text)
 
     def log(self, color, sort, text):
         time = datetime.now().time().strftime('%H:%M:%S')
-        print(f'{self.green}twitchtube {self.white}| {time} - {color + sort}{self.white}: {text}')
+        print(f'{f.GREEN}twitchtube {f.WHITE}| {time} - {color + sort}{f.WHITE}: {text}{f.WHITE}')
