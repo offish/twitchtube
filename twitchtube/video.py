445e033da43cc653bf3d3aeec3ed6a9c276c2868
offish <overutilization@gmail.com>
2020-12-05 13:23:46 +0100

    Update video.py

--- twitchtube/video.py
+++ twitchtube/video.py
@@ -1,6 +1,6 @@
 import os
 
-from .config import RESOLUTION, FRAMES
+from .config import RESOLUTION, FRAMES, FILE_NAME
 from .logging import Log
 
 from moviepy.editor import VideoFileClip, concatenate_videoclips
@@ -9,7 +9,11 @@ from moviepy.editor import VideoFileClip, concatenate_videoclips
 log = Log()
 
 
-def get_clip_files(path: str):
+def get_clip_files(path: str) -> list:
+    """
+    Gets all the mp4 files listed in the given
+    path and returns the files/paths as a list.
+    """
     clips = []
 
     for file in os.listdir(path):
@@ -18,7 +22,12 @@ def get_clip_files(path: str):
     return clips
 
 
-def render(path: str):
+def render(path: str) -> None:
+    """
+    Concatenates a video with given path.
+    Finds every mp4 file in given path, downloads
+    them and add them into a list to be rendered.
+    """
     log.info(f'Going to render video in {path}\n')
 
     videos = []
@@ -26,10 +35,11 @@ def render(path: str):
     for video in get_clip_files(path):
 
         movie = VideoFileClip(video, target_resolution=RESOLUTION)
+        # Just so we get cleaner logging
         name = video.replace(path, '') \
             .replace('_', ' ') \
             .replace('\\', '')
-
+        
         videos.append(movie)
 
         log.info(f'Added {name} to be rendered')
@@ -39,8 +49,8 @@ def render(path: str):
         del name
 
     final = concatenate_videoclips(videos, method='compose')
-    final.write_videofile(f'{path}/rendered.mp4', fps=FRAMES, \
-        temp_audiofile=f'{path}/temp-audio.m4a', remove_temp=True, \
+    final.write_videofile(f'{path}\\{FILE_NAME}.mp4', fps=FRAMES, \
+        temp_audiofile=f'{path}\\temp-audio.m4a', remove_temp=True, \
         codec="libx264", audio_codec="aac")
 
     print() # New line for cleaner logging
