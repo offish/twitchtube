dc30b25dd082f21d96b7e85826ffe9074f68684c
offish <overutilization@gmail.com>
2021-01-14 11:38:05 +0100

    Update logging.py

--- twitchtube/logging.py
+++ twitchtube/logging.py
@@ -11,18 +11,19 @@ def log(color: int, sort: str, text: str) -> None:
     """
     Used for colored printing, does not return anything.
     """
-    time = datetime.now().time().strftime('%H:%M:%S')
-    text.encode(encoding='UTF-8', errors='ignore')
-    print(f'{f.GREEN}twitchtube {f.WHITE}| {time} - {color + sort}{f.WHITE}: {text}{f.WHITE}')
+    time = datetime.now().time().strftime("%H:%M:%S")
+    text.encode(encoding="UTF-8", errors="ignore")
+    print(
+        f"{f.GREEN}twitchtube {f.WHITE}| {time} - {color + sort}{f.WHITE}: {text}{f.WHITE}"
+    )
 
 
 class Log:
-
     def info(self, text: str):
-        log(f.GREEN, 'info', text)
+        log(f.GREEN, "info", text)
 
     def error(self, text: str):
-        log(f.RED, 'error', text)
+        log(f.RED, "error", text)
 
     def warn(self, text: str):
-        log(f.MAGENTA, 'warn', text)
+        log(f.MAGENTA, "warn", text)
