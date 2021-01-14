db6e6d00199c91eba4e004125f225963e06da3c3
offish <overutilization@gmail.com>
2021-01-14 11:36:39 +0100

    Update api.py

--- twitchtube/api.py
+++ twitchtube/api.py
@@ -10,29 +10,20 @@ local = locals()
 
 def request(endpoint: str, headers: dict, params: dict) -> dict:
     return requests.get(
-        'https://api.twitch.tv/' + endpoint,
-        headers = headers,
-        params = params
+        "https://api.twitch.tv/" + endpoint, headers=headers, params=params
     )
 
 
 def data(slug: str) -> dict:
     return request(
-        'helix/clips',
-        {
-            'Authorization': f'Bearer {OAUTH_TOKEN}',
-            'Client-Id': CLIENT_ID
-        },
-        {'id': slug}
+        "helix/clips",
+        {"Authorization": f"Bearer {OAUTH_TOKEN}", "Client-Id": CLIENT_ID},
+        {"id": slug},
     )
 
 
 def top_clips(headers: dict, params: dict) -> dict:
-    return request(
-        'kraken/clips/top',
-        headers,
-        params
-    )
+    return request("kraken/clips/top", headers, params)
 
 
 def get(name: str, **args) -> dict:
