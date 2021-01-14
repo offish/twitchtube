79daa8cdbe3081edce4ce7145c19b1affe5c3b07
offish <overutilization@gmail.com>
2021-01-14 11:35:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from os.path import exists
 from pathlib import Path
 from time import sleep
-from json import dump
+from json import dump, JSONDecodeError
 from glob import glob
 from os import remove
 
@@ -15,16 +15,16 @@ from twitchtube.video import render
 
 log = Log()
 
-    
+
 while True:
 
     for game in GAMES:
-        
+
         path = CLIP_PATH.format(get_date(), game)
 
         # Here we check if we've made a video for today
         # by checking if the rendered file exists.
-        if not exists(path + f'\\{FILE_NAME}.mp4'):
+        if not exists(path + f"\\{FILE_NAME}.mp4"):
 
             # We want to retry because Twitch often gives a
             # 500 Internal Server Error when trying to get clips
@@ -38,7 +38,7 @@ while True:
 
                 # Check if the API gave us a successful response
                 if clips:
-                    log.info(f'Starting to make a video for {game}')
+                    log.info(f"Starting to make a video for {game}")
                     # Download all needed clips
                     names = download_clips(clips, VIDEO_LENGTH, path)
                     config = create_video_config(game, names)
@@ -47,35 +47,42 @@ while True:
                         render(path)
 
                     if UPLOAD_TO_YOUTUBE:
-                        upload_video_to_youtube(config)
-                    
+                        try:
+                            upload_video_to_youtube(config)
+                        except JSONDecodeError:
+                            log.error("Your client_secret is empty or has wrong syntax")
+
                     if SAVE_TO_FILE:
-                        with open(path + f'\\{SAVE_FILE_NAME}.json', 'w') as f:
+                        with open(path + f"\\{SAVE_FILE_NAME}.json", "w") as f:
                             dump(config, f, indent=4)
 
                     if DELETE_CLIPS:
                         # Get all the mp4 files in the path and delte them
                         # if they're not the rendered video
-                        files = glob(f'{path}/*.mp4')
+                        files = glob(f"{path}/*.mp4")
 
                         for file in files:
-                            if not file == path + f'\\{FILE_NAME}.mp4':
+                            if not file == path + f"\\{FILE_NAME}.mp4":
                                 try:
                                     remove(file)
                                 # Sometimes a clip is "still being used" giving
                                 # us an exception that would else crash the program
                                 except PermissionError as e:
-                                    log.error(f'Could not delete {file} because {e}')
-                    
+                                    log.error(f"Could not delete {file} because {e}")
+
                     break
 
                 else:
                     # Response was most likely an Internal Server Error and we retry
-                    log.error(f'There was an error or timeout on Twitch\'s end, retrying... {i + 1}/{RETRIES}')
+                    log.error(
+                        f"There was an error or timeout on Twitch's end, retrying... {i + 1}/{RETRIES}"
+                    )
 
         else:
             # Rendered video does already exist
-            log.info(f'Already made a video for {game}. Rechecking after {TIMEOUT} seconds.')
+            log.info(
+                f"Already made a video for {game}. Rechecking after {TIMEOUT} seconds."
+            )
 
     # Sleep for given timeout to check if it's a different date
     sleep(TIMEOUT)
