c88462aa4fffb55a5b4fc76d3a8f5ca991edd0ae
offish <overutilization@gmail.com>
2021-01-22 20:51:32 +0100

    Update utils.py

--- twitchtube/utils.py
+++ twitchtube/utils.py
@@ -11,43 +11,43 @@ def get_date() -> str:
     return date.today().strftime("%b-%d-%Y")
 
 
-def create_video_config(game: str, streamers: list) -> dict:
+def create_video_config(category: str, streamers: list) -> dict:
     """
     Creates the video config used for uploading to YouTube
     returns a dict.
     """
     return {
         "category": CATEGORY,
-        "keywords": get_tags(game),
-        "description": get_description(game, streamers),
-        "title": get_title(game),
-        "file": get_file(game),
+        "keywords": get_tags(category),
+        "description": get_description(category, streamers),
+        "title": get_title(category),
+        "file": get_file(category),
     }
 
 
-def get_tags(game: str) -> str:
+def get_tags(category: str) -> str:
     """
-    Gets the tag for given game (if any) as a string.
+    Gets the tag for given category (if any) as a string.
     """
-    return str(TAGS.get(game))
+    return str(TAGS.get(category))
 
 
-def get_description(game: str, streamers: list) -> str:
+def get_description(category: str, streamers: list) -> str:
     """
     Gets the description with given list of streamers
-    and game, returns the description as a string.
+    and category, returns the description as a string.
     """
     names = "Streamers in this video:\n"
 
     for name in streamers:
         names += f"https://twitch.tv/{name}\n"
 
-    if game in DESCRIPTIONS:
-        return DESCRIPTIONS[game].format(names)
+    if category in DESCRIPTIONS:
+        return DESCRIPTIONS[category].format(names)
     return names
 
 
-def get_title(game: str) -> str:
+def get_title(category: str) -> str:
     """
     Gets the title and returns it as a string.
     """
@@ -55,16 +55,16 @@ def get_title(game: str) -> str:
         return TITLE
 
     title = json.loads(
-        open(f"{CLIP_PATH.format(get_date(), game)}/clips.json", "r").read()
+        open(f"{CLIP_PATH.format(get_date(), category)}/clips.json", "r").read()
     )
 
     for i in title:
         # Return the first entry's title
-        return f"{title[i]['title']} - {game} Twitch Highlights"
+        return f"{title[i]['title']} - {category} Twitch Highlights"
 
 
-def get_file(game: str) -> str:
+def get_file(category: str) -> str:
     """
-    Gets the path/file given game and returns it as a string.
+    Gets the path/file given category and returns it as a string.
     """
-    return f"{CLIP_PATH.format(get_date(), game)}\\{FILE_NAME}.mp4"
+    return f"{CLIP_PATH.format(get_date(), category)}\\{FILE_NAME}.mp4"
