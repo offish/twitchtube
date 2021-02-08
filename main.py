6c5f76f57764379ae7b3b210b659c50ba95f8ba2
offish <overutilization@gmail.com>
2021-02-08 20:50:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
-from os.path import exists
 from pathlib import Path
 from time import sleep
 from json import dump, JSONDecodeError
 from glob import glob
-from os import remove
+import os
 
 from twitchtube.logging import Log
 from twitchtube.config import *
-from twitchtube.upload import Upload
-from twitchtube.utils import create_video_config, get_date
+from twitchtube.utils import create_video_config, get_path
 from twitchtube.clips import get_clips, download_clips
 from twitchtube.video import render
 from twitchtube import __name__, __version__
+
+from opplast import Upload
 
 
 log = Log()
 
 log.info(f"Running {__name__} at v{__version__}")
 
-
 while True:
 
     for category in LIST:
-
-        path = CLIP_PATH.format(get_date(), category)
+        path = get_path(category)
 
         # Here we check if we've made a video for today
         # by checking if the rendered file exists.
-        if not exists(path + f"/{FILE_NAME}.mp4"):
+        if not os.path.exists(path + f"/{FILE_NAME}.mp4"):
 
             # We want to retry because Twitch often gives a
             # 500 Internal Server Error when trying to get clips
@@ -54,13 +52,22 @@ while True:
                             dump(config, f, indent=4)
 
                     if UPLOAD_TO_YOUTUBE and RENDER_VIDEO:
-                        upload = Upload(ROOT_PROFILE_PATH, config, SLEEP)
-                        was_uploaded, video_id = upload.upload()
+                        upload = Upload(ROOT_PROFILE_PATH, SLEEP, HEADLESS, DEBUG)
 
-                        if was_uploaded:
-                            log.info(f"{video_id} was successfully uploaded to YouTube")
-                        else:
-                            log.error("Video was not successfully uploaded to YouTube")
+                        log.info("Trying to upload video to YouTube")
+
+                        try:
+                            was_uploaded, video_id = upload.upload(config)
+
+                            if was_uploaded:
+                                log.info(
+                                    f"{video_id} was successfully uploaded to YouTube"
+                                )
+
+                        except Exception as e:
+                            log.error(
+                                f"There was an error {e} when trying to upload to YouTube"
+                            )
 
                     if DELETE_CLIPS:
                         # Get all the mp4 files in the path and delte them
@@ -73,7 +80,7 @@ while True:
                                 == path + f"/{FILE_NAME}.mp4"
                             ):
                                 try:
-                                    remove(file)
+                                    os.remove(file)
                                     log.info(f"Deleted {file.replace(path, '')}")
                                 # Sometimes a clip is "still being used" giving
                                 # us an exception that would else crash the program
