6278f869c38fce842e970851f43a1cf93f285815
offish <overutilization@gmail.com>
2021-04-24 15:39:46 +0200

    Update config.py

--- twitchtube/config.py
+++ twitchtube/config.py
@@ -4,103 +4,58 @@ import pathlib
 # Changing FRAMES and or RESOULTION will heavily impact load on CPU.
 # If you have a powerful enough computer you may set it to 1080p60
 
-# Secrets
-# Twitch Client ID
-CLIENT_ID = ""
-
-# Twitch OAuth Token
-OAUTH_TOKEN = ""
-
-# Path to the Firefox profile where you are logged into YouTube
-ROOT_PROFILE_PATH = ""
-
-# Selenium
-# How many seconds Firefox should sleep for when uploading
-SLEEP = 3
-
-# If True Firefox will be hidden (True/False)
-HEADLESS = True
-
-# If information when uploading should be printed (True/False)
-DEBUG = True
-
-
-# Paths
+# other
 PATH = str(pathlib.Path().absolute()).replace("\\", "/")
 CLIP_PATH = PATH + "/clips/{}/{}"
 
 
-# If clips should be rendered into one video (True/False)
-# If set to False everything else under Video will be ignored
-RENDER_VIDEO = True
-
-# Resoultion of the rendered video (height, width) for 1080p: ((1080, 1920))
-RESOLUTION = (720, 1280)
-
-# Frames per second (30/60)
-FRAMES = 30
-
-# Minumum video length in minutes (doesn't always work)
-VIDEO_LENGTH = 10.5
-
-# Resize clips to fit RESOLUTION (True/False)
-# If any RESIZE option is set to False the video might end up having a weird resolution
-RESIZE_CLIPS = True
-
-# Name of the rendered video
-FILE_NAME = "rendered"
+# twitch
+CLIENT_ID = ""  # Twitch Client ID
+OAUTH_TOKEN = ""  # Twitch OAuth Token
+PERIOD = "day"  # day, week, month or all
+LANGUAGE = "en"  # en, es, th etc.
+LIMIT = 100  # 1-100
 
 
-# Enable (True/False)
-# Resize (True/False) read RESIZE_CLIPS
-# Path to video file (str)
-ENABLE_INTRO = False
-RESIZE_INTRO = True
-INTRO_FILE_PATH = PATH + "/assets/intro.mp4"
+# selenium
+ROOT_PROFILE_PATH = ""  # Path to the Firefox profile where you are logged into YouTube
+SLEEP = 3  # How many seconds Firefox should sleep for when uploading
+HEADLESS = True  # If True Firefox will be hidden (True/False)
+DEBUG = True  # If information when uploading should be printed (True/False)
 
+
+# video options
+RENDER_VIDEO = True  # If clips should be rendered into one video (True/False). If set to False everything else under Video will be ignored
+RESOLUTION = (
+    720,
+    1280,
+)  # Resoultion of the rendered video (height, width) for 1080p: ((1080, 1920))
+FRAMES = 30  # Frames per second (30/60)
+VIDEO_LENGTH = 10.5  # Minumum video length in minutes (doesn't always work)
+RESIZE_CLIPS = True  # Resize clips to fit RESOLUTION (True/False) If any RESIZE option is set to False the video might end up having a weird resolution
+FILE_NAME = "rendered"  # Name of the rendered video
+ENABLE_INTRO = False  # Enable (True/False)
+RESIZE_INTRO = True  # Resize (True/False) read RESIZE_CLIPS
+INTRO_FILE_PATH = PATH + "/assets/intro.mp4"  # Path to video file (str)
 ENABLE_TRANSITION = True
 RESIZE_TRANSITION = True
 TRANSITION_FILE_PATH = PATH + "/assets/transition.mp4"
-
 ENABLE_OUTRO = False
 RESIZE_OUTRO = True
 OUTRO_FILE_PATH = PATH + "/assets/outro.mp4"
 
 
-# Other
-# If YouTube stuff should be saved to a separate file e.g. title, description & tags (True/False)
-SAVE_TO_FILE = True
-
-# Name of the file YouTube stuff should be saved to
-SAVE_FILE_NAME = "youtube"
-
-# If the rendered video should be uploaded to YouTube after rendering (True/False)
-UPLOAD_TO_YOUTUBE = True
-
-# If the downloaded clips should be deleted after rendering the video (True/False)
-DELETE_CLIPS = True
-
-# How often it should check if it has made a video today (in seconds)
-TIMEOUT = 3600
+# other options
+SAVE_TO_FILE = True  # If YouTube stuff should be saved to a separate file e.g. title, description & tags (True/False)
+SAVE_FILE_NAME = "youtube"  # Name of the file YouTube stuff should be saved to
+UPLOAD_TO_YOUTUBE = True  # If the rendered video should be uploaded to YouTube after rendering (True/False)
+DELETE_CLIPS = True  # If the downloaded clips should be deleted after rendering the video (True/False)
 
 
-# Twitch
-RETRIES = 5
-
-# Twitch API Request Options
-HEADERS = {"Accept": "application/vnd.twitchtv.v5+json", "Client-ID": CLIENT_ID}
-PARAMS = {"period": "day", "language": "en", "limit": 100}  # 100 is max
-
-
-# YouTube
-# If empty, it would take the title of the first clip, and add "- *category* Highlights Twitch"
-TITLE = ""
-
-DESCRIPTION = "Streamers in this video:\n"
-
-# Thumbnails
-THUMBNAIL = ""
-
-# Tags
-
-TAGS = ["twitch", "just chatting", "xqc"]
+# youtube
+TITLE = ""  # youtube title
+DESCRIPTION = (
+    "Streamers in this video:\n"  # youtube description, streamers will be added
+)
+THUMBNAIL = ""  # path to the image file to be set as thumbnail
+TAGS = ["twitch", "just chatting", "xqc"]  # your youtube tags
