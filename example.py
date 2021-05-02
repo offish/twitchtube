aa507f16a3312ec18e7c6edf0282999fb4b48860
offish <overutilization@gmail.com>
2021-05-02 20:52:19 +0200

    Update example.py

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ from twitchtube.video import make_video
 
 
 make_video(
-    ["c xQcOW", "game Just Chatting"],
+    data=["c xQcOW", "game Just Chatting"],
     client_id="1hq8ektpki36w5kn37mluioungyqjo",  # example client id (fake)
     oauth_token="9f5einm9qtp0bj4m9l1ykevpwdn98o",  # example token (fake)
     video_length=10.5,
